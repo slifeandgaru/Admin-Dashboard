a8995f79ad3759f020416e36768f7522171fed81
KazumaSatou <req.dieter@gmail.com>
2023-08-07 11:41:50 +0700

    remove buy Pro

---
+++ config/connectDB.js
@@ -0,0 +1,7 @@
+// const mongoose = require('mongoose')
+// mongoose.connect('mongodb+srv://truongnx28062000:Ivw4449zUqhswUoS@cluster0.0qv4t8g.mongodb.net/Last_Project?retryWrites=true&w=majority',{
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true
+// })
+
+// module.exports = mongoose
